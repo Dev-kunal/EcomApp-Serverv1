ba9c6ca1d654409e2d629b8f5b9fbe97df8677af
kunal7722 <dev35.programmingtree@gmail.com>
2021-05-09 20:25:51 +0530

    add route not found handler,use .env

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,15 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const initializeDBConnection=async()=>{
-  try{
-  await mongoose.connect("mongodb+srv://kunaltijare:kunaltijarecluster@cluster0.i2ze7.mongodb.net/inventory?retryWrites=true&w=majority", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  console.log("mongoose connected successfully")
-  }catch(err){
-      console.error("mongoose connection failed...", err)
+const initializeDBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_HOST, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("mongoose connected successfully");
+  } catch (err) {
+    console.error("mongoose connection failed...", err);
   }
-}
+};
 
-module.exports = { initializeDBConnection }
+module.exports = { initializeDBConnection };
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const express=require("express");
-const bodyParser=require("body-parser");
-const mongoose=require("mongoose");
-const cors=require("cors");
-
-
-const app=express();
-app.use(bodyParser.json({urlExtended:true}))
+const app = express();
+app.use(bodyParser.json({ urlExtended: true }));
 app.use(cors());
-const productRouter=require("./routes/product.router.js");
-const userRouter=require("./routes/user.router");
-const cartRouter=require("./routes/cart.router");
-const wishlistRouter=require("./routes/wishlist.router");
+const productRouter = require("./routes/product.router.js");
+const userRouter = require("./routes/user.router");
+const cartRouter = require("./routes/cart.router");
+const wishlistRouter = require("./routes/wishlist.router");
 
-const { initializeDBConnection } = require("./db/db.connect.js")
+const { initializeDBConnection } = require("./db/db.connect.js");
 initializeDBConnection();
 
+app.use("/products", productRouter);
+app.use("/users", userRouter);
+app.use("/cart", cartRouter);
+app.use("/wishlist", wishlistRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "WEl-Come" });
+});
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "route not found on server, please check",
+  });
+});
 
-app.use("/products",productRouter);
-app.use("/users",userRouter)
-app.use("/cart",cartRouter)
-app.use("/wishlist",wishlistRouter)
-app.get("/",(req,res)=>{
-  res.json({message:"WEl-Come"})
-})
-app.listen(3000,()=>{
- console.log("Server Started")
- })
- 
+app.listen(3000, () => {
+  console.log("Server Started");
+});
